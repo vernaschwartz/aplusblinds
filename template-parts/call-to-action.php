e658d3c1e6763434b3f98e537c1e4bb3db3e09a6
vernaschwartz <45664373+vernaschwartz@users.noreply.github.com>
2019-04-25 10:08:28 -0400

    changed color of CTA on all pages

--- template-parts/call-to-action.php
+++ template-parts/call-to-action.php
@@ -1,10 +1,10 @@
 
     <!--CTA-->
-    <section id="CTA" style="background-image:  linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.4)), url(<?php bloginfo('template_directory'); ?>/images/productsimg.jpg); height: 250px; background-repeat: no-repeat; background-size: cover; background-position: center center;">
+    <section id="CTA" style="background-color: #ea1f25; height: 250px; background-repeat: no-repeat; background-size: cover; background-position: center center;">
         <div class="container-fluid">
             <h2 >We've got you covered!</h2>
             <p >Let's talk! Call Now at (317) 698-5445</p>
-            <a class="btn btn-primary " style="border-color: #ea1f25;"  href="<?php echo esc_url( home_url( '/' ) ); ?>contact-us/">Schedule a Free Consultation</a>
+            <a class="btn btn-primary " style="border-color: #2c2c2c; background: #2c2c2c;"  href="<?php echo esc_url( home_url( '/' ) ); ?>contact-us/">Schedule a Free Consultation</a>
         </div>
         <!--/.container-->
     </section>
